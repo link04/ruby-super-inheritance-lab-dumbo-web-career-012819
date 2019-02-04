e278be99103dbe04e701669e9048c19faf0fbf7d
Maximo Bautista <max.bautista97@gmail.com>
2019-02-04 03:35:44 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,25 @@ class ChattyStudent < Student
   def raise_hand
     super
     puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+     super
+    puts "Pick me!"
+    
   end
   
   
